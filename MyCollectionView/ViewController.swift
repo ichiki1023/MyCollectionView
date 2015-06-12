80b8156a7acc51c2200cb576e94524ff3d3304b4
佐藤一輝 <i.tennis0210@gmail.com>
2015-06-12 14:12:03 +0900

    Initial Commit

---
+++ MyCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCollectionView
+//
+//  Created by 佐藤一輝 on 2015/06/12.
+//  Copyright (c) 2015年 ichiki sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
